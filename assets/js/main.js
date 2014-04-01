82ffd096808c6b24f3badfa1ace2a1ffcb908619
Michiel Waaijer <michiel.waaijer@gmail.com>
2014-04-01 16:00:40 +0800

    added prototype v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 $( document ).ready(function() {
-    console.log( "ready! hallalla" );
-
-    $('.nav-tabs a').click(function (e) {
-		  e.preventDefault()
-		  $(this).tab('show')
-		});
-
-		$('.gallery').colorbox({rel:'gal'});
+    $(".link").click(function() {
+    	var scrn = $(this).attr('data-img');
+    	var z = $(this).attr('data-z');
+    	
+    	if (z == undefined) z = 1;
+    	console.log(z);
+    	$('.screenwrapper img').attr('src', scrn + '.jpg');
+    	$('.screenwrapper').css('z-index',z);
+    	//console.log(scrn);
+    })
 });
